74b189d06786ec09f7763981d6c27162a80eab8a
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:20:38 +0300

    deneme.txt dosyasının içindeki kelime sayısı karakter ve cümle sayısını ekrana yazdırma

--- main.c
+++ main.c
@@ -1,49 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include<ctype.h>
 int main()
 {
-    char buf[100],gec[100],ad[20],ad2[20],sesli[10]="aAeEiIoOuU";
-    int say=0,i,j,a;
-    FILE *dosya,*dosya2;
-    printf("Dosyanin adini giriniz:");
+    char ad[20],buf[100];
+    int i,cumleS=0,kelimeS=0,karakterS=0;
+    printf("Hangi dosya hakkinda bilgi verilecek:");
     gets(ad);
-    strcpy(ad2,ad);
-    strcat(ad2,".ssz");
+    FILE *dosya;
     dosya=fopen(ad,"r");
-    dosya2=fopen(ad2,"w");
     if(dosya==NULL)
     {
-        printf("\n%s ADLI DOSYA ACILAMADI!!!",ad);
+        printf("\nDOSYA ACILAMADI!!!");
         exit(1);
     }
-    if(dosya2==NULL)
+    while((fgets(buf,100,dosya))!=NULL)
     {
-        printf("\n%s ADLI DOSYA ACILAMADI!!!",ad2);
-        exit(1);
-    }
-    while(fgets(buf,100,dosya)!=NULL)
-    {
-        a=0;
-        for(j=0;buf[j]!='\0';j++)
+        for(i=0;buf[i]!='\0';i++)
         {
-             say=0;
-             for(i=0;i<10;i++)
-             {
-                 if(buf[j]==sesli[i])
-                    say++;
-             }
-             if(say==0)
-             {
-                 gec[a]=buf[j];
-                 a++;
-             }
+            if(buf[i]=='.')
+                cumleS++;
+            else if(buf[i]==' ')
+                kelimeS++;
+            karakterS++;
         }
-        printf("\ngec:%s\n",gec);
-        fprintf(dosya2,"%s",gec);
     }
-    printf("\n\nISLEM BITTI!!!");
-    fclose(dosya2);
+    printf("\n\nCumle sayisi:%d\n",cumleS);
+    printf("Kelime sayisi:%d\n",kelimeS+cumleS);
+    printf("Karakter sayisi:%d\n",karakterS);
     fclose(dosya);
     return 0;
 }
