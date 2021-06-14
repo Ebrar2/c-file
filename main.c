e9793db0121545efc7c3b6a2dbcb364429a7d8f4
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 18:11:37 +0300

    Kart numarasına göre  casusu bulma
    Bir yere girilirken kart numarsı ile giriş yapılmaktadır.Bu yerde çalışan birinin kart numarası çalınmıştır bu programla 1 den fazla girişleri casus olarak kabul etmektedir

--- main.c
+++ main.c
@@ -1,72 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-#include<ctype.h>
-struct kalori
-{
-    char yiyecek[20];
-    float kalori;
-};
-struct siparis
-{
-    char yiyecek[20];
-    float adet;
-};
+#define dosyaA "giris.txt"
 int main()
 {
     FILE *dosya,*dosya2;
-    float toplamKalr=0.0;
-    char buf[100],*p;
-    int a=0,b=0,i,j=0;
-    dosya=fopen("kalori.txt","r");
-    dosya2=fopen("siparis.txt","r");
+    char buf[50],kartno[50][50];
+    int a=0,i=0,j=0;
+    dosya=fopen(dosyaA,"r");
     if(dosya==NULL)
     {
-        printf("\nkalori.txt ADLI DOSYA ACILAMADI!!!");
+        printf("DOSYA ACILAMADI!!!");
         exit(1);
     }
+    dosya2=fopen("casus.txt","w");
     if(dosya2==NULL)
     {
-        printf("\nsiparis.txt ADLI DOSYA ACILAMADI!!!");
+        printf("DOSYA ACILAMADI!!!");
         exit(1);
     }
-    while(fgets(buf,100,dosya)!=NULL)
+    while(fgets(buf,50,dosya)!=NULL)
+    {
+        strcpy(kartno[a],buf);
+        printf("i:%d kartno:%s\n",a,kartno[a]);
         a++;
-    fseek(dosya,0,SEEK_SET);
-    struct kalori k[a];
-    while(fgets(buf,100,dosya2)!=NULL)
-        b++;
-    fseek(dosya2,0,SEEK_SET);
-    struct siparis s[b];
-    while(fgets(buf,100,dosya)!=NULL)
-    {
-        p=strtok(buf," ");
-        strcpy(k[j].yiyecek,p);
-        p=strtok(NULL," ");
-        k[j].kalori=atof(p);
-       // printf("\nk[j].yiyecek:%s    k[j].kalori:%.2f",k[j].yiyecek,k[j].kalori);
-        j++;
     }
-    j=0;
-    printf("\n");
-    while(fgets(buf,100,dosya2)!=NULL)
+    for(i=0;i<a;i++)
     {
-        p=strtok(buf," ");
-        strcpy(s[j].yiyecek,p);
-        p=strtok(NULL," ");
-        s[j].adet=atof(p);
-       // printf("\ns[j].yiyecek:%s s[j]:%.2f",s[j].yiyecek,s[j].adet);
-        for(i=0;i<a;i++)
+        printf("\nk[i]:%s",kartno[i]);
+        for(j=i+1;j<a;j++)
         {
-            if(strcmp(s[j].yiyecek,k[i].yiyecek)==0)
-            {
-                toplamKalr=k[i].kalori*s[j].adet+toplamKalr;
-                break;
+            if(strcmp(kartno[i],kartno[j])==0)
+              {
+                  printf("\nAYNI:%s",kartno[j]);
+                  fprintf(dosya2,"%s\n",kartno[j]);
+              }
             }
-        }
-        j++;
     }
-    printf("\n\n\nTOPLAM KALORI:%.2f",toplamKalr);
+    printf("\nISLEM BITTI!!!");
     fclose(dosya2);
     fclose(dosya);
     return 0;
