76f47506634ec6cee5c71415d159b57b072f6768
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 17:56:51 +0300

    Fiyat txt dosyasındaki telefonları ucuz pahalı olarak txlere ayırma
    Fiyat txt dosyasındaki telefonları eğer ortalamanın altında ise ya da eşitse ucuz üstünde ise pahalı txt e atan program

--- main.c
+++ main.c
@@ -2,97 +2,76 @@
 #include <stdlib.h>
 #include<string.h>
 #include<ctype.h>
-#define dosyaA "istek.txt"
-#define dosyaA2 "depo.txt"
-struct siparis
+struct telefon
 {
     char ad[20];
-    int miktar;
+    char ad2[20];
+    float fiyat;
 };
-void ayir(char *buf,struct siparis *s)
+void ayir(struct telefon *t,char *buf)
 {
     char *p;
     p=strtok(buf," ");
-    strcpy(s->ad,p);
+    strcpy(t->ad,p);
     p=strtok(NULL," ");
-    s->miktar=atoi(p);
+    strcpy(t->ad2,p);
+    p=strtok(NULL," ");
+    t->fiyat=atof(p);
+}
+void ucuzPahali(struct telefon t[],int n)
+{
+    FILE *ucuz,*pahali;
+    int i;
+    float top=0.0,ort;
+    ucuz=fopen("ucuz.txt","w");
+    if(ucuz==NULL)
+    {
+        printf("\n\nucuz.txt ADLI DOSYA ACILAMADI");
+        exit(1);
+    }
+    pahali=fopen("pahali.txt","w");
+    if(pahali==NULL)
+    {
+        printf("\n\npahali.txt ADLI DOSYA ACILAMADI");
+        exit(1);
+    }
+    for(i=0;i<n;i++)
+        top=top+t[i].fiyat;
+    ort=top/n;
+    printf("\nort:%.2f",ort);
+    for(i=0;i<n;i++)
+    {
+        if(t[i].fiyat<=ort)
+            fprintf(ucuz,"%s %s %.2f\n",t[i].ad,t[i].ad2,t[i].fiyat);
+        else
+            fprintf(pahali,"%s %s %.2f\n",t[i].ad,t[i].ad2,t[i].fiyat);
 
+    }
+    fclose(ucuz);
+    fclose(pahali);
 }
 int main()
 {
-    FILE *dosya,*dosya2,*yeni;
+    FILE *fiyat;
     char buf[100];
-    int a=0,b=0,i=0,j,fark;
-    dosya=fopen(dosyaA,"r");
-    if(dosya==NULL)
+    int a=0,i=0;
+    fiyat=fopen("fiyat.txt","r");
+    if(fiyat==NULL)
     {
-        printf("%s ADLI DOSYA ACILAMADI",dosyaA);
+        printf("fiyat.txt ADLI DOSYA ACILAMADI");
         exit(1);
     }
-    dosya2=fopen(dosyaA2,"r");
-    if(dosya2==NULL)
-    {
-        printf("%s ADLI DOSYA ACILAMADI",dosyaA2);
-        exit(1);
-    }
-    while(fgets(buf,100,dosya)!=NULL)
+    while(fgets(buf,100,fiyat)!=NULL)
         a++;
-    fseek(dosya,0,SEEK_SET);
-    struct siparis s[a];
-    while(fgets(buf,100,dosya)!=NULL)
+    struct telefon t[a];
+    fseek(fiyat,0,SEEK_SET);
+    while(fgets(buf,100,fiyat)!=NULL)
     {
-        ayir(buf,s+i);
+        ayir(t+i,buf);
         i++;
     }
-     while(fgets(buf,100,dosya2)!=NULL)
-        b++;
-    fseek(dosya2,0,SEEK_SET);
-    struct siparis depo[b];
-    i=0;
-    while(fgets(buf,100,dosya2)!=NULL)
-    {
-        ayir(buf,depo+i);
-       // printf("\ni:%d DEPO:%s   %d",i,depo[i].ad,depo[i].miktar);
-        i++;
-    }
-    for(i=0;i<a;i++)
-    {
-        for(j=i+1;j<a;j++)
-        {
-            if(strcmp(s[i].ad,s[j].ad)==0)
-            {
-                strcpy(s[j].ad,"TEKRAR");
-                s[i].miktar=s[i].miktar+s[j].miktar;
-                s[j].miktar=0;
-            }
-        }
-    }
-    yeni=fopen("nekaldi.txt","w");
-    if(yeni==NULL)
-    {
-        printf("\n\nnekaldi.txt ADLI DOSYA ACILAMADI");
-        exit(1);
-    }
-    for(i=0;i<b;i++)
-    {
-            for(j=0;j<a;j++)
-            {
-                if(strcmp(depo[i].ad,s[j].ad)==0)
-                    {
-                       // printf("\nDEPO:%s   %d",depo[i].ad,depo[i].miktar);
-                        //printf("\nISTEK:%s   %d",s[j].ad,s[j].miktar);
-                        fark=depo[i].miktar-s[j].miktar;
-                        if(fark<0)
-                           fprintf(yeni,"%s karsilanamaz\n",depo[i].ad);
-                        else
-                            fprintf(yeni,"%s %d\n",depo[i].ad,fark);
-                        break;
-                    }
-            }
-    }
-    printf("\nISLEM BITTI");
-    fclose(yeni);
-    fclose(dosya2);
-    fclose(dosya);
+    ucuzPahali(t,a);
+    fclose(fiyat);
+    printf("\n\nISLEM BITTT");
     return 0;
 }
